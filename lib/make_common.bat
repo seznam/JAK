5b9c91036d557b08a5f804aafa3e8b955d1ef416
Ondřej Žára <ondrej.zara@firma.seznam.cz>
2007-10-25 11:49:12 +0000

    #164 object.js do common.js

--- lib/make_common.bat
+++ lib/make_common.bat
@@ -1,2 +1,2 @@
-rem Zretezeni sesti nejpouzivanejsich knihoven do jedne
-copy /b main.js + class_maker.js + browser.js + utils.js  + events.js + dom.js common.js
+rem Zretezeni sedmi nejpouzivanejsich knihoven do jedne
+copy /b main.js + object.js + class_maker.js + browser.js + utils.js  + events.js + dom.js common.js
