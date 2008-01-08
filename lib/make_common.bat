5f3f443b379d119d1c42dd3990e65f3f167a9362
Ondřej Žára <ondrej.zara@firma.seznam.cz>
2008-01-08 14:52:12 +0000

    #164 error ven z baliku, docfix v events

--- lib/make_common.bat
+++ lib/make_common.bat
@@ -1,2 +1,2 @@
 rem Zretezeni vsech knihoven do jedne
-copy /b main.js + class_maker.js + events.js + browser.js + components.js + dom.js + error.js + object.js + request.js + sig_interface.js + signals.js common.js
+copy /b main.js + class_maker.js + events.js + browser.js + components.js + dom.js + object.js + request.js + sig_interface.js + signals.js common.js
