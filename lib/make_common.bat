77e41eba2480f5fbdf09a74718d8b39c7246f670
Michal Aichinger <michal.aichinger@firma.seznam.cz>
2008-01-29 13:10:00 +0000

    #164 ne do jaku

--- lib/make_common.bat
+++
@@ -1,2 +0,0 @@
-rem Zretezeni vsech knihoven do jedne
-copy /b main.js + class_maker.js + events.js + browser.js + components.js + dom.js + object.js + request.js + sig_interface.js + signals.js common.js
