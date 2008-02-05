a4afee8bbb8c3adf83746ceb540f1dacd402638b
Ondřej Žára <ondrej.zara@firma.seznam.cz>
2008-02-05 10:39:48 +0000

    #164 velke prejmenovani c.2

--- lib/makecommon.bat
+++ lib/makecommon.bat
@@ -1,2 +1,2 @@
 rem Zretezeni vsech knihoven do jedne
-copy /b main.js + class_maker.js + events.js + browser.js + components.js + dom.js + object.js + request.js + sig_interface.js + signals.js common.js
+copy /b main.js + classmaker.js + events.js + browser.js + components.js + dom.js + object.js + request.js + siginterface.js + signals.js common.js
