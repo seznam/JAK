a73e208e0109688f4c5b8ebdf1658a84b269736b
Ondřej Žára <ondrej.zara@firma.seznam.cz>
2008-03-28 14:13:25 +0000

    #164 zmena vystupu parseru

--- util/parser.js
+++ util/parser.js
@@ -17,9 +17,19 @@ SZN.Parser = SZN.ClassMaker.makeClass({
 /**
  * @method 
  * @param {string} str retezec, jenz mame naparsovat
- * @returns {date || false} datum, pokud lze. V opacnem pripade false
+ * @returns {object || false} literalovy objekt, pokud lze. V opacnem pripade false
  */
 SZN.Parser.date = function(str) {
+	var obj = {
+		year:0,
+		month:0,
+		day:0,
+		hours:0,
+		minutes:0,
+		seconds:0,
+		milliseconds:0,
+	}
+	
 	var separators = "[\-/\\\\:.]";
 	var chars = "[0-9]";
 	var patterns = [
@@ -37,91 +47,89 @@ SZN.Parser.date = function(str) {
 	}
 	if (!result) { return false; }
 	
-	var parsedDate = new Date(0);
-	var a = Math.round(parseFloat(result[1]));
-	var b = Math.round(parseFloat(result[2]));
-	var c = Math.round(parseFloat(result[3]));
+	var a = parseInt(result[1],10);
+	var b = parseInt(result[2],10);
+	var c = parseInt(result[3],10);
 	var yearIndex = -1;
 	if (result[1].length == 4) {
 		yearIndex = 0;
 	} else if (result[3].length == 4) {
 		yearIndex = 2;
 	} else {
+		var y = (new Date()).getFullYear();
 		if (a > 31) {
-			a = a + (a > parsedDate.getFullYear()-2000 ? 1900 : 2000);
+			a += (a > y-2000 ? 1900 : 2000);
 			yearIndex = 0;
 		} else {
-			c = c + (c > parsedDate.getFullYear()-2000 ? 1900 : 2000);
+			c += (c > y-2000 ? 1900 : 2000);
 			yearIndex = 2;
 		}
 	}
 
 	if (yearIndex == 0) { /* year at the beginning */
-		parsedDate.setFullYear(a);
-		parsedDate.setDate(1);
+		obj.year = a;
 		var max = Math.max(b,c);
 		var min = Math.min(b,c);
 		if (max > 13) {
-			parsedDate.setMonth(min-1);
-			parsedDate.setDate(max);
+			obj.month = min-1;
+			obj.day = max;
 		} else {
-			parsedDate.setMonth(b-1);
-			parsedDate.setDate(c);
+			obj.month = b-1;
+			obj.day = c;
 		}
 	} else if (yearIndex == 2) { /* year at the end */
-		parsedDate.setFullYear(c);
+		obj.year = c;
 		var max = Math.max(a,b);
 		var min = Math.min(a,b);
 		if (max > 13) {
-			parsedDate.setMonth(min-1);
-			parsedDate.setDate(max);
+			obj.month = min-1;
+			obj.day = max;
 		} else {
-			parsedDate.setMonth(b-1);
-			parsedDate.setDate(a);
+			obj.month = b-1;
+			obj.day = a;
 		}
 	} /* year at the end */
 	
 	/* time */
 	if (result[4]) {
-		var h = parseInt(result[4].match(/[0-9]+/)[0],10);
-		var m = (result[5] ? parseInt(result[5].match(/[0-9]+/)[0],10) : 0);
-		var s = (result[6] ? parseInt(result[6].match(/[0-9]+/)[0],10) : 0);
-		parsedDate.setHours(h);
-		parsedDate.setMinutes(m);
-		parsedDate.setSeconds(s);
+		obj.hours = parseInt(result[4].match(/[0-9]+/)[0],10);
+		obj.minutes = (result[5] ? parseInt(result[5].match(/[0-9]+/)[0],10) : 0);
+		obj.seconds = (result[6] ? parseInt(result[6].match(/[0-9]+/)[0],10) : 0);
 	}
 
-	return parsedDate;
+	return obj;
 };
 
 /**
  * @method 
  * @param {string} str retezec, jenz mame naparsovat
- * @returns {array || false} trojprvkove pole [r, g, b] (kazde 0-1), pokud lze. V opacnem pripade false
+ * @returns {object || false} literalovy objekt, pokud lze. V opacnem pripade false
  */
 SZN.Parser.color = function(str) {
+	var obj = {r:0, g:0, b:0};
+
 	if (str.indexOf("#") != -1) { /* hex */
 		var regs = str.match(/ *#([0-9]+)/);
 		if (!regs) { return false; }
 		var c = regs[1];
 		if (c.length == 6) {
-			var r = parseInt(c.slice(0,2),16)/255;
-			var g = parseInt(c.slice(2,4),16)/255;
-			var b = parseInt(c.slice(4,6),16)/255;
-			return [r,g,b];
+			obj.r = parseInt(c.slice(0,2),16);
+			obj.g = parseInt(c.slice(2,4),16);
+			obj.b = parseInt(c.slice(4,6),16);
+			return obj;
 		} else if (c.length == 3) {
-			var r = parseInt(c.charAt(0),16)*17/255;
-			var g = parseInt(c.charAt(1),16)*17/255;
-			var b = parseInt(c.charAt(2),16)*17/255;
-			return [r,g,b]
+			obj.r = parseInt(c.charAt(0),16)*17;
+			obj.g = parseInt(c.charAt(1),16)*17;
+			obj.b = parseInt(c.charAt(2),16)*17;
+			return obj;
 		} else { return false; }
 	} else { /* dec */
 		var regs = str.match(/ *\( *([0-9]+) *, *([0-9]+) *, *([0-9]+)/);
 		if (!regs) { return false; }
-		r = parseInt(regs[1],10)/255;
-		g = parseInt(regs[2],10)/255;
-		b = parseInt(regs[3],10)/255;
-		return [r,g,b];
+		obj.r = parseInt(regs[1],10);
+		obj.g = parseInt(regs[2],10);
+		obj.b = parseInt(regs[3],10);
+		return obj;
 	}
 }
 
@@ -131,8 +139,16 @@ SZN.Parser.color = function(str) {
  * @returns {string || false} cast s validni emailovou adresou, pokud lze. V opacnem pripade false
  */
 SZN.Parser.email = function(str) {
-	var regs = str.match(/[a-z][a-z0-9.-_]*@[a-z0-9][a-z0-9.-_]*.[a-z]{2,5}/i);
+	var obj = {
+		mailbox:"",
+		domain:"",
+		tld:""
+	}
+	var regs = str.match(/([a-z][a-z0-9.-_]*)@([a-z0-9][a-z0-9.-_]*)\.([a-z]{2,5})/i);
 	if (regs) {
-		return regs[0];
+		obj.mailbox = regs[1];
+		obj.domain = regs[2];
+		obj.tld = regs[3];
 	} else { return false; }
+	return obj;
 }
