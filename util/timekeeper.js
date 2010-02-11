e43924a6061db962f02fd69d892ad74334d329e7
Ondřej Žára <ondrej.zara@firma.seznam.cz>
2010-02-11 13:10:12 +0100

    timekeeper utility singleton

---
+++ util/timekeeper.js
@@ -0,0 +1,74 @@
+/**
+ * @class namespace Knihovna kompresních funkcí
+ * @group jak-utils
+ */
+JAK.Timekeeper = JAK.ClassMaker.makeSingleton({
+	NAME: "JAK.Timekeeper",
+	VERSION: "1.0"
+});
+
+JAK.Timekeeper.prototype.$constructor = function() {
+	this._delay = 20;
+	this._listeners = [];
+	this._interval = null;
+	this._tick = this._tick.bind(this);
+}
+
+/**
+ * Přidání posluchače
+ * @param {object} what Objekt žádající o notifikaci
+ * @param {string || function} method Metoda k volání
+ * @param {int} [count=1] Počet tiknutí na jednu notifikaci
+ */
+JAK.Timekeeper.prototype.addListener = function(what, method, count) {
+	var index = this._findListener(what);
+	if (index != -1) { throw new Error("This listener is already attached"); }
+	
+	var obj = {
+		what: what,
+		method: method,
+		count: count || 1,
+		bucket: 0
+	}
+	obj.bucket = obj.count;
+	this._listeners.push(obj);
+	
+	if (!this._interval) { this._interval = setInterval(this._tick, this._delay); }
+	return this;
+}
+
+/**
+ * Odebrání posluchače
+ * @param {object} what Objekt žádající o odebrání
+ */
+JAK.Timekeeper.prototype.removeListener = function(what) {
+	var index = this._findListener(what);
+	if (index == -1) { throw new Error("Cannot find listener to be removed"); }
+	this._listeners.splice(index, 1);
+	
+	if (!this._listeners.length) { 
+		clearInterval(this._interval);
+		this._interval = null;
+	}
+	return this;
+}
+
+JAK.Timekeeper.prototype._findListener = function(what) {
+	for (var i=0;i<this._listeners.length;i++) {
+		if (this._listeners[i].what == what) { return i; }
+	}
+	return -1;
+}
+
+JAK.Timekeeper.prototype._tick = function() {
+	for (var i=0;i<this._listeners.length;i++) {
+		var item = this._listeners[i];
+		item.bucket--;
+		if (item.bucket) { continue; } /* jeste ne */
+		
+		item.bucket = item.count;
+		var obj = item.what;
+		var method = (typeof(item.method) == "string" ? obj[item.method] : item.method);
+		method.call(obj);
+	}
+}
