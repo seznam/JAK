857cbd493eee7e1edf5f9f9bed7c8247f34342fb
Ondřej Žára <ondrej.zara@firma.seznam.cz>
2010-06-29 17:01:07 +0200

    xmlrpc in jak.xml

--- util/xml.js
+++ util/xml.js
@@ -4,7 +4,7 @@
 */
 
 /**
- * @overview Prace s XML
+ * @overview Práce s XML
  * @author Zara
  */ 
 
@@ -14,7 +14,7 @@
  */
 JAK.XML = JAK.ClassMaker.makeStatic({
 	NAME: "JAK.XML",
-	VERSION: "2.1"
+	VERSION: "2.2"
 });
 
 /**
@@ -56,4 +56,73 @@ JAK.XML.createDocument = function(xmlStr) {
 	} else {
 		throw new Error("No XML parser available");
 	}
+}
+
+
+/**
+ * Parsování XMLRPC
+ * @namespace
+ * @group jak-utils
+ */
+JAK.XML.RPC = JAK.ClassMaker.makeStatic({
+	NAME: "JAK.XML.RPC",
+	VERSION: "1.0"
+});
+
+/**
+ * XML na objekt
+ * @param {node} node
+ * @returns {object}
+ */
+JAK.XML.RPC.xmlToObject = function(node) {
+	return this._structToObject(node);
+}
+
+JAK.XML.RPC._valueToObject = function(node) {
+	switch (node.nodeName.toLowerCase()) {
+		case "string":
+			return node.firstChild.nodeValue;
+		break;
+		case "i4":
+		case "int":
+			return parseInt(node.firstChild.nodeValue);
+		break;
+		case "double":
+			return parseFloat(node.firstChild.nodeValue);
+		break;
+		case "boolean":
+			return (node.firstChild.nodeValue == "1");
+		break;
+		case "array":
+			return this._arrayToObject(node);
+		break;
+		case "struct":
+			return this._structToObject(node);
+		break;
+	}
+}
+
+JAK.XML.RPC._arrayToObject = function(node) {
+	var arr = [];
+	var data = JAK.XML.childElements(node)[0];
+	var values = JAK.XML.childElements(data);
+	for (var i=0;i<values.length;i++) {
+		var value = JAK.XML.childElements(values[i])[0];
+		arr.push(this._valueToObject(value));
+	}
+	return arr;
+}
+
+JAK.XML.RPC._structToObject = function(node) {
+	var obj = {};
+	var members = JAK.XML.childElements(node);
+	for (var i=0;i<members.length;i++) {
+		var member = members[i];
+		var namevalue = JAK.XML.childElements(member);
+		
+		var name = namevalue[0].firstChild.nodeValue;
+		var value = JAK.XML.childElements(namevalue[1])[0];
+		obj[name] = this._(value);
+	}
+	return obj;
 }
