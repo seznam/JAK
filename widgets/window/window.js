734a247cd5c0d59334d47735bfc6b23c6c68ad80
Ondřej Žára <ondrej.zara@firma.seznam.cz>
2010-10-22 11:37:09 +0200

    window with modifyable images

--- widgets/window/window.js
+++ widgets/window/window.js
@@ -5,7 +5,7 @@
 
 /**
  * @overview window
- * @version 2.0
+ * @version 2.1
  * @author zara
 */   
 
@@ -15,7 +15,7 @@
  */
 JAK.Window = JAK.ClassMaker.makeClass({
 	NAME: "Window",
-	VERSION: "2.0"
+	VERSION: "2.1"
 });
 
 /**
@@ -25,12 +25,12 @@ JAK.Window = JAK.ClassMaker.makeClass({
  * @param {int[]} [optObj.sizes=[6,6,6,6]] Pole ctyr velikosti okraju, dle hodinovych rucicek
  */
 JAK.Window.prototype.$constructor = function(optObj) {
-	this.options = {
+	this._options = {
 		imagePath:"/img/shadow-",
 		imageFormat:"png",
 		sizes:[6,6,6,6]
 	}
-	for (var p in optObj) { this.options[p] = optObj[p]; }
+	for (var p in optObj) { this._options[p] = optObj[p]; }
 
 	/**
 	 * @field content vnitrni bunka, do ktere se da pridavat dalsi obsah
@@ -40,48 +40,46 @@ JAK.Window.prototype.$constructor = function(optObj) {
 	 * @field vnejsi prvek
 	 */
 	this.container = false;
-	this._buildDom();
-}
-
-/**
- * Tvorba DOM stromu
- */
-JAK.Window.prototype._buildDom = function() {
-	var imageNames = [
+	
+	this._nodes = [];
+	
+	this._imageNames = [
 		["lt","t","rt"],
 		["l","","r"],
 		["lb","b","rb"]
-	]
+	];
+
+	this._build();
+}
+
+/**
+ * Tvorba DOM stromu - tabulky
+ */
+JAK.Window.prototype._build = function() {
 	this.container = JAK.mel("div", {className:"window-container"}, {position:"relative",zIndex:10});
 	var table = JAK.mel("table", null, {borderCollapse:"collapse",position:"relative"});
 	var tbody = JAK.mel("tbody");
 	JAK.DOM.append([table,tbody],[this.container,table]);
 	
 	for (var i=0;i<3;i++) {
+		this._nodes.push([]);
 		var tr = JAK.cel("tr");
 		tbody.appendChild(tr);
 		for (var j=0;j<3;j++) {
 			var td = JAK.cel("td");
+			this._nodes[i].push(td);
 			td.style.padding = "0px";
 			td.style.margin = "0px";
 			var div = (i == 1 && j == 1 ? this.content : JAK.mel("div", null, {overflow:"hidden"}));
 			td.appendChild(div);
 			
-			var im = imageNames[i][j];
-			if (im) { /* image */
-				var path = this.options.imagePath + im + "." + this.options.imageFormat;
-				if (JAK.Browser.client == "ie" && JAK.Browser.version < 7 && this.options.imageFormat.match(/png/i)) {
-					td.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+path+"',sizingMethod='scale')";
-				} else {
-					td.style.backgroundImage = "url("+path+")";
-				} /* not ie */
-			}
+			this.setImage(i, j);
 			
 			/* dimensions */
-			if (i == 0) { div.style.height = this.options.sizes[0]+"px"; }
-			if (i == 2) { div.style.height = this.options.sizes[2]+"px"; }
-			if (j == 0) { div.style.width = this.options.sizes[3]+"px"; }
-			if (j == 2) { div.style.width = this.options.sizes[1]+"px"; }
+			if (i == 0) { div.style.height = this._options.sizes[0]+"px"; }
+			if (i == 2) { div.style.height = this._options.sizes[2]+"px"; }
+			if (j == 0) { div.style.width = this._options.sizes[3]+"px"; }
+			if (j == 2) { div.style.width = this._options.sizes[1]+"px"; }
 			if (j == 1 && i != 1) { td.style.width = "auto"; }
 			
 			tr.appendChild(td);
@@ -100,7 +98,6 @@ JAK.Window.prototype.$destructor = function() {
  * Ukazani okna
  */
 JAK.Window.prototype.show = function() {
-	//this.container.style.visibility = "visible";
 	this.container.style.display = "";
 }
 
@@ -108,7 +105,19 @@ JAK.Window.prototype.show = function() {
  * Schovani okna
  */
 JAK.Window.prototype.hide = function() {
-	//this.container.style.visibility = "hidden";
 	this.container.style.display = "none";
 }
 
+JAK.Window.prototype.setImage = function(row, col, image) {
+	var img = this._imageNames[row][col] + (image || "");
+	if (!img) { return; }
+	
+	var td = this._nodes[row][col];
+
+	var path = this._options.imagePath + img + "." + this._options.imageFormat;
+	if (JAK.Browser.client == "ie" && JAK.Browser.version < 7 && this.options.imageFormat.match(/png/i)) {
+		td.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+path+"',sizingMethod='scale')";
+	} else {
+		td.style.backgroundImage = "url("+path+")";
+	}
+}
