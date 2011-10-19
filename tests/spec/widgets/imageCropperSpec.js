e101d36168e625f358be79afb26675de0610a988
Marek Fojtl <jerrymf@gmail.com>
2011-10-19 21:58:49 +0200

    Unit test for ImageCropper widget

---
+++ tests/spec/widgets/imageCropperSpec.js
@@ -0,0 +1,12 @@
+describe("ImageCropper", function() {
+	it("should create instance of ImageCropper", function() {
+	  var img = JAK.cel("img");
+	  var f = JAK.cel("form");
+	  
+	  document.body.appendChild(img);
+	  document.body.appendChild(f);
+	  
+	  var iC = new JAK.ImageCropper(img, f);
+	  expect(iC instanceof JAK.ImageCropper).toEqual(true);
+	});
+});
