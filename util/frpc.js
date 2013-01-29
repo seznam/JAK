c79a691f1147b22ec411114ce62fcf2d0f59d050
Ondrej Zara <ondrej.zara@firma.seznam.cz>
2013-01-29 15:53:15 +0100

    frpc ignores >3byte utf8 values

--- util/frpc.js
+++ util/frpc.js
@@ -266,11 +266,14 @@ JAK.FRPC._decodeUTF8 = function(length) {
 			pointer += 1; /* FIXME safari bug */
 			result += SfCC(((c & 31) << 6) | (c2 & 63));
 			remain -= 1;
-		} else {
+		} else if (c < 240) {
 			c2 = data[pointer++];
 			c3 = data[pointer++];
 			result += SfCC(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
 			remain -= 2;
+		} else { /* 4+ byte stuff, throw away */
+			pointer += 3;
+			remain -= 3;
 		}
 		
 		/* pokud bylo na vstupu nevalidni UTF-8, mohli jsme podlezt... */
