47f85e9b915980a1c5794e42376c1da45610136f
Ondrej Zara <ondrej.zara@firma.seznam.cz>
2013-04-22 14:41:55 +0200

    fix for string passed as EXTEND

--- tests/spec/libs/classmaker/classMakerExtendSpec.js
+++ tests/spec/libs/classmaker/classMakerExtendSpec.js
@@ -1,54 +1,59 @@
 describe('ClassMaker extend tests', function(){
-    it('should fail when we try to instantiate class "Trida", which extend "Test" which is defined later', function(){
-        function tryCatch() {
-            var SZN;
-            try {
-                SZN = {};
+	it('should fail when we try to instantiate class "Trida", which extend "Test" which is defined later', function(){
+		function tryCatch() {
+			var SZN;
+			try {
+				SZN = {};
 
-                SZN.Trida = JAK.ClassMaker.makeClass({
-                    NAME: 'SZN.Trida',
-                    VERSION: '1.0',
-                    EXTEND: SZN.Test
-                });
+				SZN.Trida = JAK.ClassMaker.makeClass({
+					NAME: 'SZN.Trida',
+					VERSION: '1.0',
+					EXTEND: SZN.Test
+				});
 
 
-                SZN.Trida.prototype.$constructor = function() {};
+				SZN.Trida.prototype.$constructor = function() {};
 
-                SZN.Test = JAK.ClassMaker.makeClass({
-                    NAME: 'SZN.Test',
-                    VERSION: '1.0'
-                });
+				SZN.Test = JAK.ClassMaker.makeClass({
+					NAME: 'SZN.Test',
+					VERSION: '1.0'
+				});
 
-                var t = new SZN.Trida();
-                return true;
-            } catch (e) {
-                return false;
-            }
-        }
+				var t = new SZN.Trida();
+				return true;
+			} catch (e) {
+				return false;
+			}
+		}
 
-        expect(tryCatch()).toBe(false);
-    });
+		expect(tryCatch()).toBe(false);
+	});
 			
-    it('should be not possible extends class which is not created by JAK',function() {
-        function tryCatch() {
-            var SZN;
-            try {
-                SZN = {};
-                //nonSZN constructor
-                SZN.Best = function() {this.a = 'test'; };
-                //extending nonSZN constructor
-                SZN.Rest = JAK.ClassMaker.makeClass({
-                    NAME: 'SZN.Rest',
-                    VERSION: '1.0',
-                    EXTEND: SZN.Best
-                });
+	it('should be not possible to extend class which is not created by JAK', function() {
+		function tryCatch() {
+			var SZN = {};
+			//nonSZN constructor
+			SZN.Best = function() {this.a = 'test'; };
+			//extending nonSZN constructor
+			SZN.Rest = JAK.ClassMaker.makeClass({
+				NAME: 'SZN.Rest',
+				VERSION: '1.0',
+				EXTEND: SZN.Best
+			});
+		}
 
-                return true;
-            } catch (e) {
-                return false;
-            }
-        }
+		expect(tryCatch).toThrow();
+	});
 
-        expect(tryCatch()).toBe(false);
-    });
+	it('should be not possible to extend non-object',function() {
+		function tryCatch() {
+			var X = JAK.ClassMaker.makeClass({
+				NAME: 'X',
+				VERSION: '1.0',
+				EXTEND: "Y"
+			});
+		}
+
+		expect(tryCatch).toThrow("Cannot extend non-function");
+	});
 });
