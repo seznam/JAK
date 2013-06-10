6020a752c2d80018c9396c385767bffbc0fef88b
Ondrej Zara <ondrej.zara@firma.seznam.cz>
2013-06-10 13:44:14 +0200

    move promise to lib

--- lib/makejak.bat
+++ lib/makejak.bat
@@ -1,2 +1,2 @@
 rem Zretezeni vsech knihoven do jedne
-copy /b main.js + classmaker.js + events.js + browser.js + dom.js + object.js + request.js + isignals.js + signals.js + decorator.js + idecorable.js + timekeeper.js +console.js jak.js
+copy /b main.js + classmaker.js + events.js + browser.js + dom.js + object.js + request.js + isignals.js + signals.js + decorator.js + idecorable.js + timekeeper.js + console.js + promise.js jak.js
