82207fc83078f0c550b8241a6a327c0a03657980
Ondrej Zara <ondrej.zara@firma.seznam.cz>
2013-07-03 16:49:43 +0200

    opravy typovych anotaci

--- bin/codecheck.sh
+++ bin/codecheck.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-cat $* |
+echo "var JAK,JSON,console,history,localStorage,VBS_getByte,VBS_getLength;" |
+cat - $* |
 	sed -r \
 		-e "s/(@(param|returns).*)([ \{])function/\1\3Function/" \
-		-e "s/(@(param|returns).*)([ \{])([a-zA-Z0-9\.]+)\[\]/\1\3Array.\4/" \
-		-e "s/(@(param|returns).*)([ \{])([a-zA-Z0-9\.]+)\[\]/\1\3Array.\4/" \
-		-e "s/(@param.*\} )\[(.*)\]/\1\2/" \
-		| closure-compiler.sh --warning_level VERBOSE --jscomp_off=globalThis --js_output_file /dev/null
+		-e "s/(@(param|returns).*)([ \{])object/\1\3Object/" \
+		-e "s/(@(param|returns).*)([ \{])array(.*\})/\1\3Array\4/" \
+		-e "s/(@(param|returns).*)([ \{])integer/\1\3number/" \
+		-e "s/(@(param|returns).*)([ \{])int/\1\3number/" \
+		-e "s/(@(param|returns).*)([ \{])bool([| \}])/\1\3boolean\4/" \
+		-e "s/(@(param|returns).*)([ \{])([a-zA-Z0-9\.]+)\[\]/\1\3Array.<\4>/" \
+		-e "s/(@(param|returns).*)([ \{])([a-zA-Z0-9\.]+)\[\]/\1\3Array.<\4>/" \
+		-e "s/(@param \{(.*))\} \[(.*)\]/\1=} \3/" \
+		| closure-compiler.sh --warning_level VERBOSE --jscomp_off=globalThis --js_output_file /dev/null 2>&1 
