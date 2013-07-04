e53b301439e38c209426893fa12d94a0a38bf62b
Ondrej Zara <ondrej.zara@firma.seznam.cz>
2013-07-04 14:45:39 +0200

    codecheck ignores unknown jsdoc tags

--- bin/codecheck.sh
+++ bin/codecheck.sh
@@ -9,11 +9,11 @@ cat - $* |
 		-e "s/(@(param|returns).*)([ \{])node(.*\})/\1\3Node\4/" \
 		-e "s/(@(param|returns).*)([ \{])array(.*\})/\1\3Array\4/" \
 		-e "s/(@(param|returns).*)([ \{])integer/\1\3number/" \
-		-e "s/(@(param|returns).*)([ \{])int([ \}])/\1\3number\4/" \
+		-e "s/(@(param|returns).*)([ \{])int([ \}\[])/\1\3number\4/" \
 		-e "s/(@(param|returns).*)([ \{])float/\1\3number/" \
 		-e "s/(@(param|returns).*)([ \{])bool([| \}])/\1\3boolean\4/" \
 		-e "s/(@(param|returns).*)([ \{])([a-zA-Z0-9\.]+)\[\]/\1\3Array.<\4>/" \
 		-e "s/(@(param|returns).*)([ \{])([a-zA-Z0-9\.]+)\[\]/\1\3Array.<\4>/" \
 		-e "s/(@param \{(.*))\} \[(.*)\]/\1=} \3/" \
 		-e "s/(@class.*)/\1\n@constructor/" \
-		| closure-compiler.sh --warning_level VERBOSE --jscomp_off=globalThis --jscomp_off=duplicate --jscomp_off=missingProperties --js_output_file /dev/null 2>&1 
+		| closure-compiler.sh --warning_level VERBOSE --jscomp_off=globalThis --jscomp_off=duplicate --jscomp_off=missingProperties --jscomp_off=nonStandardJsDocs --js_output_file /dev/null 2>&1 
